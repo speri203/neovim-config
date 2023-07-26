c94dda907b826663d0d0a912266739c3aa400031
speri <peri.sai203@gmail.com>
2023-07-26 16:17:11 +0530

    LSP related plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,12 @@
 return {
-    -- Add the community repository of plugin specifications
-    "AstroNvim/astrocommunity",
-    -- example of imporing a plugin, comment out to use it or add your own
-    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-    { import = "astrocommunity.pack.lua" },
-    { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
 }
